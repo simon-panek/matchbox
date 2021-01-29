ffacd790f7d5bf5fd57645c0879d4b76b4862784
Simon Panek <paneks19@gmail.com>
2021-01-28 16:55:12 -0800

    Updates

--- static/js/main.b13ae13d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpmatchbox=this.webpackJsonpmatchbox||[]).push([[0],{10:function(t,e,c){"use strict";c.r(e);var n=c(0),s=c(1),o=c.n(s),r=c(3),a=c.n(r);c(9);var i=function(){return Object(n.jsx)("div",{className:"App"})};a.a.render(Object(n.jsx)(o.a.StrictMode,{children:Object(n.jsx)(i,{})}),document.getElementById("root"))},9:function(t,e,c){}},[[10,1,2]]]);
-//# sourceMappingURL=main.b13ae13d.chunk.js.map
